101aef2636c572623b377d099ca180f3d2da3b72
Derek Walsh <derek.walsh@gmail.com>
2022-06-09 15:01:18 -0700

    Testing NavBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import "@aws-amplify/ui-react/styles.css";
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
+import { NavBar } from "./ui-components";
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AmplifySignOut />
         <header className="App-header">
+            <NavBar />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -30,4 +33,9 @@ class App extends Component {
   }
 }
 
+
+
+
+
 export default withAuthenticator(App);
+//export default App
--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//const root = document.getElementById('root');
+//ReactDOM.render(<App />, root);
+
+const root = document.getElementById('root');
+
+function Welcome(props) {
+    const element =
+    (
+         <div>
+            <h1>Hello, {props.firstname} {props.lastname}!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+     <App />
+         </div>
+        
+     );
+  return element;
+}
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+    const welcomeHeader = <Welcome firstname="Derek" lastname="Walsh" />;
+    //ReactDOM.render(element, root);
+    ReactDOM.render(<App />, root);
+}
+
+ReactDOM.render(<App />, root);
+
+//setInterval(tick, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
